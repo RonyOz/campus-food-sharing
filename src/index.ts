bc16a2e1930a14c6f6768137527701a6fd5e352b
ronyoz <ronyfarido@gmail.com>
2025-09-27 15:08:04 -0500

    Initialize Express app with user, order, and product routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import express, { type Express } from "express";
+import { userRouter } from "./routes/user.route";
+import { orderRouter } from "./routes/order.route";
+import { productRouter } from "./routes/product.route";
+
+const app: Express = express();
+
+const port: number = 3000;
+
+const baseUrl: string = '/api/v1';
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+app.use(`${baseUrl}/users`, userRouter)
+app.use(`${baseUrl}/orders`, orderRouter)
+app.use(`${baseUrl}/products`, productRouter)
+
+
+// db.then(()=>{
+//     app.listen(port, ()=>{
+//         console.log(`Server running on ${port} port`)
+//     })
+// })
