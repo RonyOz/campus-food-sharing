c0c1960bd925acf1d5f6fa8bcfd48de23d0d9116
ronyoz <ronyfarido@gmail.com>
2025-09-27 18:30:48 -0500

    swagger no swaggerea

---
+++ src/lib/swagger.ts
@@ -0,0 +1,25 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc ={
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Campus Food Sharing API',
+			version: '1.0.0',
+			description: 'API para la gestión de usuarios, productos y pedidos.',
+		},
+		servers: [
+			{
+				url: 'http://localhost:3000',
+				description: 'Servidor local',
+			},
+		],
+	},
+	apis: ['../routes/*.ts'], 
+}
+
+const outputFile = '../swagger-output.json'; 
+const endpointsFiles = ['../routes/*.ts'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
