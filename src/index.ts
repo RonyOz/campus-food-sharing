bb2e2874d89982b930776cfc35b350ae3ee0c138
ronyoz <ronyfarido@gmail.com>
2025-09-27 18:31:15 -0500

    Refactor Swagger setup and route imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,18 @@
 import express, { type Express } from "express";
-import { userRouter } from "./routes/user.route";
-import { orderRouter } from "./routes/order.route";
-import { productRouter } from "./routes/product.route";
 import { mongoDB } from "./lib/connectionDB";
+import { orderRouter, productRouter, userRouter } from "./routes";
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
 const app: Express = express();
-
 const port: number = 3000;
-
 const baseUrl: string = '/api/v1';
-
-// Swagger setup
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-
-const swaggerSpecs = swaggerJsdoc({
-	definition: {
-		openapi: '3.0.0',
-		info: {
-			title: 'Campus Food Sharing API',
-			version: '1.0.0',
-			description: 'API para la gestión de usuarios, productos y pedidos.',
-		},
-		servers: [
-			{
-				url: 'http://localhost:3000',
-				description: 'Servidor local',
-			},
-		],
-	},
-	apis: ['./routes/*.ts'], 
-});
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
 app.use(`${baseUrl}/users`, userRouter)
