38a2b6c26c9746a5016fba9ea5c52511ab8f9f6e
ronyoz <ronyfarido@gmail.com>
2025-09-27 18:02:48 -0500

    Set up Swagger documentation for the Campus Food Sharing API

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { type Express } from "express";
 import { userRouter } from "./routes/user.route";
 import { orderRouter } from "./routes/order.route";
 import { productRouter } from "./routes/product.route";
+import { mongoDB } from "./lib/connectionDB";
 
 const app: Express = express();
 
@@ -9,20 +10,40 @@ const port: number = 3000;
 
 const baseUrl: string = '/api/v1';
 
-app.use(express.urlencoded({ extended: false}));
-app.use(express.json());
+// Swagger setup
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+const swaggerSpecs = swaggerJsdoc({
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Campus Food Sharing API',
+			version: '1.0.0',
+			description: 'API para la gestión de usuarios, productos y pedidos.',
+		},
+		servers: [
+			{
+				url: 'http://localhost:3000',
+				description: 'Servidor local',
+			},
+		],
+	},
+	apis: ['./routes/*.ts'], 
+});
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+
+// Routes
 app.use(`${baseUrl}/users`, userRouter)
 app.use(`${baseUrl}/orders`, orderRouter)
 app.use(`${baseUrl}/products`, productRouter)
 
-app.listen(port, ()=>{
-    console.log(`Server running on ${port} port`)
+mongoDB.then(() => {
+	app.listen(port, () => {
+		console.log(`Server running on ${port} port`)
+	})
 })
-
-
-// db.then(()=>{
-//     app.listen(port, ()=>{
-//         console.log(`Server running on ${port} port`)
-//     })
-// })
