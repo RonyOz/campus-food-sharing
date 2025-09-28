78b6896794e6f8270c48b6f6722e19bbff9493a4
ronyoz <ronyfarido@gmail.com>
2025-09-27 20:23:52 -0500

    Add login and signup routes to userRouter

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,6 +3,9 @@ import { userController } from "../controllers/user.controller";
 
 export const userRouter = Router();
 
+userRouter.post("/login", userController.login);
+userRouter.post("/signup", userController.signup);
+
 userRouter.get("/", userController.getAllUsers);
 userRouter.get("/:id", userController.getUserById);
 userRouter.post("/", userController.createUser);
