50cad22c4ee5437976840a97fb4ad094a80d0813
ronyoz <ronyfarido@gmail.com>
2025-09-27 20:45:39 -0500

    Add authentication middleware to order, product, and user routes

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { productController } from "../controllers/product.controller";
+import { auth } from "../middlewares/auth.middleware";
 
 export const productRouter = Router();
 
 productRouter.get("/", productController.getAllProducts);
 productRouter.get("/:id", productController.getProductById);
-productRouter.post("/", productController.createProduct);
-productRouter.put("/:id", productController.updateProduct);
-productRouter.delete("/:id", productController.deleteProduct);
+productRouter.post("/", productController.createProduct,auth);
+productRouter.put("/:id", productController.updateProduct,auth);
+productRouter.delete("/:id", productController.deleteProduct,auth);
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { userController } from "../controllers/user.controller";
+import { auth, authorizeRoles } from "../middlewares/auth.middleware";
 
 export const userRouter = Router();
 
 userRouter.post("/login", userController.login);
 userRouter.post("/signup", userController.signup);
 
-userRouter.get("/", userController.getAllUsers);
-userRouter.get("/:id", userController.getUserById);
-userRouter.post("/", userController.createUser);
-userRouter.put("/:id", userController.updateUser);
-userRouter.delete("/:id", userController.deleteUser);
+userRouter.get("/", userController.getAllUsers,auth,authorizeRoles(['admin']));
+userRouter.get("/:id", userController.getUserById,auth);
+userRouter.put("/:id", userController.updateUser,auth);
+userRouter.delete("/:id", userController.deleteUser,auth,authorizeRoles(['admin']));
