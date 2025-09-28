2abca79ee578503888cd2ec013d878cfafa68111
ronyoz <ronyfarido@gmail.com>
2025-09-27 20:23:33 -0500

    Add SecurityService for password encryption and JWT token generation

---
+++ src/services/security.service.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+class SecurityService {
+    async encryptPassword(password: string) {
+        return await bcrypt.hash(password, 10);
+    }
+
+    async generateToken(_id: mongoose.Types.ObjectId, email: string, role: string) {
+        return await jwt.sign({ _id, email, role }, 'secret', { expiresIn: '1h' });
+    }
+    
+    async comparePassword(incommingPassword: string, currentPassword: string) {
+        return await bcrypt.compare(incommingPassword, currentPassword);
+    }
+}
+
+export const securityService = new SecurityService();
