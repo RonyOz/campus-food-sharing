1dcd837d903f579e4e8aa01acf0bc73ea42e06e8
ronyoz <ronyfarido@gmail.com>
2025-09-27 20:22:57 -0500

    Add authentication and role authorization middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,30 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import type { UserRole } from "../models/user.model";
+
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    let token = req.header('Authorization');
+    if (!token) {
+      res.status(401).json({ message: 'Unauthorized' });
+      return;
+    }
+    token = token.replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'secret');
+    req.body.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).json(error);
+  }
+}
+
+export const authorizeRoles = (allowRoles: UserRole[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.body.user;
+    if (user && !allowRoles.includes(user.role)) {
+      res.status(403).json({ message: `Forbidden, you are a ${user.role} and this service is only available for ${allowRoles}` });
+      return;
+    }
+    next();
+  }
+} 
