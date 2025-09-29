ed585538657d9abfd443c19ff9e0222ba0f9edc9
ronyoz <ronyfarido@gmail.com>
2025-09-28 21:29:44 -0500

    implement getAllUsers method in UserController for user retrieval

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,16 @@
 import type { Request, Response } from "express";
 import { userService } from "../services/user.service";
-import type { UserDocument } from "../models/user.model";
-
 
 class UserController {
 
-	async getAllUsers(req: Request, res: Response) { }
+	async getAllUsers(req: Request, res: Response) {
+		try {
+			const users = await userService.getAllUsers();
+			res.status(200).json(users);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	 }
 
 	async getUserById(req: Request, res: Response) { }
 
